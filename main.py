50206e3c66744ae88799c46782cc7ff3b60b6694
naas <naas@maul.ru>
2022-03-29 11:48:09 +0700

    Управление exception

--- main.py
+++ main.py
@@ -5,27 +5,13 @@ import logging
 import yaml
 from pymysql.cursors import DictCursor
 
+with open('device_listen.conf', "r") as f:  # загружаем настройки
+    conf = yaml.safe_load(f)
 logger = logging.getLogger('device_listen')
 
 
 def device_listen():
     try:
-        with open('device_listen.conf', "r") as f:  # загружаем настройки
-            conf = yaml.safe_load(f)
-
-        # настраиваем логгинг
-        logger.setLevel(logging.DEBUG)
-        ch = logging.StreamHandler()
-        hf = logging.FileHandler(conf['app']['log_file'])
-        hf.setLevel(logging.DEBUG)
-        ch.setLevel(logging.DEBUG)
-        formatter = logging.Formatter('%(asctime)s [%(levelname)s] [%(process)s] %(message)s', '%H:%M:%S')
-        hf.setFormatter(formatter)
-        ch.setFormatter(formatter)
-        logger.addHandler(ch)
-        logger.addHandler(hf)
-        logger.info('Run')
-
         with pymysql.connect(
             host=conf['db']['host'],
             user=conf['db']['user'],
@@ -54,8 +40,10 @@ def device_listen():
                             with connection.cursor() as cur_update:
                                 cur_update.execute(query)
                         row = cur_select.fetchone()
-                    except all:
-                        logger.error("Can't connect to device to send command")
+                        response.close()
+                    except requests.exceptions.RequestException as e:
+                        connection.close()
+                        logger.error("Can't connect to device to send command: " + format(e))
                         time.sleep(conf['app']['delay'])
                         return 1
                 connection.commit()
@@ -68,47 +56,67 @@ def device_listen():
                     try:
                         response = requests.get('http://%s/all' % (row['address']))  # получаем данные с устройства
                         logger.info(response.text)
-                    except all:
+                        response.close()
+                    except requests.exceptions.RequestException as e:
+                        connection.close()
                         logger.error("Can't connect to device")
                         time.sleep(conf['app']['delay'])
                         return 1
 
                     # записываем данные в таблицу info
-                    query = "INSERT INTO info (date, id_device, json) VALUES(CURRENT_TIMESTAMP, %s, '%s')" \
+                    query = "INSERT INTO info (date, id_device, json) VALUES (CURRENT_TIMESTAMP, %s, '%s')" \
                             % (row['id_device'], response.text)
                     with connection.cursor() as cur_insert:
                         cur_insert.execute(query)
 
                     # загружаем перечень датчиков устройства
                     data = response.json()
-                    query = "SELECT id_sensor, name FROM sensor WHERE id_device=1"
+                    query = "SELECT id_sensor, name FROM sensor WHERE id_device=%s"
                     with connection.cursor() as cur_sens:
-                        cur_sens.execute(query)
+                        cur_sens.execute(query, row['id_device'])
                     for row_sens in cur_sens.fetchall():    # по каждому датчику
                         # записываем данные в таблицу data
                         d = data[row_sens['name']]
                         if isinstance(d, (int, float)): # если данные корректные
-                            query = "INSERT INTO sensor_data (date, id_sensor, data_float) VALUES(CURRENT_TIMESTAMP, %s, IFNULL(%s,0))" \
-                                    % (row_sens['id_sensor'], d)
-                            with connection.cursor() as cur_insert:
-                                cur_insert.execute(query)
+                            query = "INSERT INTO sensor_data (date, id_sensor, data_float) " \
+                                    "VALUES (CURRENT_TIMESTAMP, %s, IFNULL(%s,0))" % (row_sens['id_sensor'], d)
+                            try:
+                                with connection.cursor() as cur_insert:
+                                    cur_insert.execute(query)
+                            except pymysql.Error as e:
+                                logger.error("Insert error:" + format(e))
+                                time.sleep(conf['app']['delay'])
+                                return 1
                         else:
                             logger.error("Wrong data: " + str(d))
-                    connection.commit()
-                    time.sleep(conf['app']['delay'])
-        return 0
+                connection.commit()
+                time.sleep(conf['app']['delay'])
 
     except pymysql.Error as e:
-        print(e)
+        connection.close()
         logger.error('Connection error: ' + format(e))
         time.sleep(conf['app']['delay'])
         return 1
 
 
 def main():
+    # настраиваем логгинг
+    logger.setLevel(logging.DEBUG)
+    ch = logging.StreamHandler()
+    hf = logging.FileHandler(conf['app']['log_file'])
+    hf.setLevel(logging.DEBUG)
+    ch.setLevel(logging.DEBUG)
+    formatter = logging.Formatter('%(asctime)s [%(levelname)s] [%(process)s] %(message)s', '%H:%M:%S')
+    hf.setFormatter(formatter)
+    ch.setFormatter(formatter)
+    logger.addHandler(ch)
+    logger.addHandler(hf)
+    logger.info('Run')
+
     # запускаем сервер
     while device_listen():
         logger.info("Restart function")
+        time.sleep(conf['app']['delay'])
 
 
 main()
